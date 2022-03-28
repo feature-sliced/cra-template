c327ba455d157cedc487278678a7605afe0ff4b1
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-29 01:15:56 +0300

    chore(ts): remove redundant app

--- typescript/template/src/app/App.jsx
+++
@@ -1,4 +0,0 @@
-import { PublicRoutes } from 'pages/router';
-import { withProviders } from './providers';
-
-export const App = withProviders(() => <PublicRoutes />);
--- typescript/template/src/app/App.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-import { PublicRoutes } from 'pages/router';
-import { withProviders } from './providers';
-
-export const App: FC = withProviders(() => <PublicRoutes />);
