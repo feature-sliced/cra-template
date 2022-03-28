178bc052f1575ca3d974f20942b0ce98b6e357b5
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-29 01:20:33 +0300

    chore(ts): simplify publicRoutes

--- typescript/template/src/pages/router/public-routes.tsx
+++ typescript/template/src/pages/router/public-routes.tsx
@@ -1,9 +1,5 @@
-import { FC } from 'react';
+import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { publicRoutes } from './routes';
 
-export const PublicRoutes: FC = () => {
-  const routes = useRoutes(publicRoutes);
-
-  return routes;
-};
+export const PublicRoutes: React.FC = () => useRoutes(publicRoutes)
