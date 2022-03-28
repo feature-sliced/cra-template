947c31e49cc004e9825269f8862929f02907b85c
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-29 01:22:01 +0300

    chore(ts): button refinement

--- typescript/template/src/shared/ui/button/button.tsx
+++ typescript/template/src/shared/ui/button/button.tsx
@@ -1,19 +1,17 @@
-import { FC, MouseEventHandler } from 'react';
+import React from 'react';
 import styles from './button.module.css';
 
 interface Props {
   className?: string;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button: FC<Props> = ({ className, children, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className={`${styles.button} ${className ? className : ''}`}
-      // instead of this className defining you can use clsx/classnames or any other lib
-    >
-      {children}
-    </button>
-  );
-};
+export const Button: React.FC<Props> = ({ className, children, onClick }) => (
+  <button
+    onClick={onClick}
+    className={`${styles.button} ${className ? className : ''}`}
+    // instead of this className defining you can use clsx/classnames or any other lib
+  >
+    {children}
+  </button>
+);
